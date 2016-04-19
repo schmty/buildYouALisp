85ac80b3b8ed4673bb7457aaf708ce58c471a019
Jake Schmitz <jake.d.schmitz@gmail.com>
2016-04-19 15:54:17 -0700

    add basic lisp math evaluation

--- parse.c
+++ parse.c
@@ -174,6 +174,114 @@ void lval_print(lval* v) {
 // lval print line
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+lval* lval_pop(lval* v, int i) {
+    // find the item at i
+    lval* x = v->cell[i];
+
+    // shift memory after the item at "i" over the top
+    memmove(&v->cell[i], &v->cell[i+1],
+            sizeof(lval*) * (v->count-i-1));
+
+    // decrease the count of items in list
+    v->count--;
+
+    // reallocate memory used
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return x;
+}
+
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
+// builtin op descriptions
+lval* builtin_op(lval* a, char* op) {
+    // ensure all arguments are numbers
+    for (int i = 0; i < a->count; i++) {
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Cannot operate on non-number!");
+        }
+    }
+
+    // pop the first element
+    lval* x = lval_pop(a, 0);
+
+    // if no arguments and sub the perform unary negation
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = -x->num;
+    }
+
+    // while there are still elements remaining
+    while (a->count > 0) {
+
+        // pop the next element
+        lval* y = lval_pop(a, 0);
+
+        // using += -= *= /= because of continual looping through list of
+        // multiple arguments
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+            if (y->num == 0) {
+                lval_del(x);
+                lval_del(y);
+                x = lval_err("Division by Zero!"); break;
+            }
+            x->num /= y->num;
+        }
+
+        lval_del(y);
+    }
+
+    lval_del(a);
+    return x;
+}
+
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) {
+
+    // evaluate children
+    for (int i = 0; i < v->count; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    // error checking
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+    }
+
+    // empty expression
+    if (v->count == 0) { return v; }
+
+    // single expression
+    if (v->count == 1) { return lval_take(v, 0); }
+
+    // ensure first element is symbol
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f);
+        lval_del(v);
+        return lval_err("S-expression Does not start with symbol");
+    }
+
+    // call builtin with operator
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    // evaluate sexprs
+    if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+    // all other lval types remain the same
+    return v;
+}
+
 int main(int argc, char** argv) {
     // create some parsers
     mpc_parser_t* Number = mpc_new("number");
@@ -209,7 +317,7 @@ int main(int argc, char** argv) {
 
         if (mpc_parse("<stdin>", input, Slither, &r)) {
             // on success print the evaluation
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
         } else {

cadd5ea28b85a38afc09511e52c0522fd17810ef
Jake Schmitz <jake.d.schmitz@gmail.com>
2016-04-18 22:31:51 -0700

    evaluate basic polish notation math

--- parsing.c
+++ parsing.c
@@ -28,6 +28,38 @@ void add_history(char* unused) {}
 #include <readline/history.h>
 #endif
 
+// eval operators
+// GUESS WHAT YOU DO THE MATH
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+// recursive eval
+// traverses ast and evaluates
+long eval(mpc_ast_t* t) {
+    // if tagged as number return it directly
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // the operator is always second child
+    char* op = t->children[1]->contents;
+
+    // store the third child in x
+    long x = eval(t->children[2]);
+
+    // iterate the remaining children and combining
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
 
 int main(int argc, char** argv) {
     // create some parsers
@@ -62,8 +94,9 @@ int main(int argc, char** argv) {
         // attempt to parse user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Slither, &r)) {
-            // on success print the ast
-            mpc_ast_print(r.output);
+            // on success print the evaluation
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             // otherwise print the error

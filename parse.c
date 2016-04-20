fd745b9d4447b42f047d4098c386be6a2f72fe40
Jake Schmitz <jake.d.schmitz@gmail.com>
2016-04-20 10:11:20 -0700

    update version

--- parse.c
+++ parse.c
@@ -422,7 +422,7 @@ int main(int argc, char** argv) {
             Number, Symbol, Sexpr, Qexpr, Expr, Slither);
 
     /* Print version and exit info */
-    puts("Slither version 0.0.7");
+    puts("Slither version 0.0.8");
     puts("Press ctrl+c to exit\n");
 
     // in a never ending loop
